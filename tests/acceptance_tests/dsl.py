d409e10c8da66a8c33f770aaa2397bcef7fd25d3
lukeblom07 <lukeblom07@gmail.com>
2025-11-25 16:01:26 +0000

    can_edit_entry function added

--- tests/acceptance_tests/dsl.py
+++ tests/acceptance_tests/dsl.py
@@ -103,7 +103,7 @@ class Dsl:
         self.driver.confirm_can_view_entry(
             register=self._encode_alias(register), entry_name=self._encode_alias(entry_name)
         
-    def update_existing_entry(self, current_name=DEFAULT_REGISTER_NAME, new_name=""):
+    def update_existing_entry(self, current_name=DEFAULT_ENTRY_NAME, new_name=""):
         current_name_alias = self._encode_alias(current_name)
         new_name_alias = self._encode_alias(new_name)
         self.driver.update_existing_entry(current_name_alias, new_name_alias)
