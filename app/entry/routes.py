82c3bd1a6d935b53a851d7f359efe77018c662d5
lukeblom07 <lukeblom07@gmail.com>
2025-11-26 11:21:32 +0000

    formatting

--- app/entry/routes.py
+++ app/entry/routes.py
@@ -14,7 +14,7 @@ typical CRUD pattern:
 
 from uuid import UUID
 
-from flask import flash, redirect, render_template, url_for, request
+from flask import flash, redirect, render_template, request, url_for
 from werkzeug import Response
 
 from app import db
