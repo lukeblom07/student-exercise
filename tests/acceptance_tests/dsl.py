82b9e7f55d4c790884efdadd032a59040bb78c88
lukeblom07 <lukeblom07@gmail.com>
2025-11-26 09:10:33 +0000

    dsl tweaks

--- tests/acceptance_tests/dsl.py
+++ tests/acceptance_tests/dsl.py
@@ -103,12 +103,12 @@ class Dsl:
         self.driver.confirm_can_view_entry(
             register=self._encode_alias(register), entry_name=self._encode_alias(entry_name)
         
-    def update_existing_entry(self, current_name=DEFAULT_ENTRY_NAME, new_name=""):
+    def update_existing_entry(self, register=DEFAULT_REGISTER_NAME, current_name=DEFAULT_ENTRY_NAME, new_name=""):
         current_name_alias = self._encode_alias(current_name)
         new_name_alias = self._encode_alias(new_name)
-        self.driver.update_existing_entry(current_name_alias, new_name_alias)
+        self.driver.update_existing_entry(register, current_name_alias, new_name_alias)
         
-    def confirm_entry_updated(self, old_name = DEFAULT_ENTRY_NAME, new_name=""):
+    def confirm_entry_updated(self, register=DEFAULT_REGISTER_NAME, old_name = DEFAULT_ENTRY_NAME, new_name=""):
         old_name_alias = self._decode_alias(old_name)
         new_name_alias = self._decode_alias(new_name)
         self.driver.confirm_entry_updated(old_name_alias, new_name_alias)
